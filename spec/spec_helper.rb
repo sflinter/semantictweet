c90de5d033d1579897d4712fb931b5b4028f0a87
Steve Flinter <stephen@flinter.com>
2010-04-10 00:27:14 +0100

    Replaced rubygems requirement

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,4 @@
+require 'rubygems'
 require 'sinatra'
 require 'spec'
 require 'spec/interop/test'
@@ -12,6 +13,5 @@ set :logging, true
 require 'application'
 
 Spec::Runner.configure do |config|
-  # reset database before each example is run
-#   config.before(:each) { DataMapper.auto_migrate! }
+
 end
