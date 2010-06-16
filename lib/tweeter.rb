6df3e8d788ac22ce8b0ba5396f7aa27fbe27c216
Steve Flinter <stephen@flinter.com>
2010-06-16 22:25:13 +0100

    Replaced geonames call

--- lib/tweeter.rb
+++ lib/tweeter.rb
@@ -8,12 +8,12 @@ class Tweeter
   include HTTParty
   base_uri APP_CONFIG[:twitter][:base_uri]
   basic_auth APP_CONFIG[:twitter][:basic_auth_username], APP_CONFIG[:twitter][:basic_auth_password]
-  
+
   attr_reader :foafs, :geoname
-  
+
   def initialize(tweeter, who = 'friends')
     @foafs = []
-    
+
     case tweeter
     when String
       @given_screen_name = URI.escape(tweeter)
@@ -21,43 +21,43 @@ class Tweeter
       if self.exists?
         foafs = self.who(who)
         @foafs = foafs.map { |foaf| Tweeter.new(foaf) }
-#         @geoname = GeoNames.new(@tweeter['location']) unless @tweeter['location'].blank?
+        @geoname = GeoNames.new(@tweeter['location']) unless @tweeter['location'].blank?
       end
     when Hash
       @tweeter = tweeter
     end
-    
+
     # This is somewhat of an ugly hack. The alternative is to put this
     # logic in method_missing()
     @tweeter['url'] = '' unless @tweeter['url']
   end
-  
+
   def exists?
     @exists
   end
-  
+
   def show
     puts "Calling: #{APP_CONFIG[:twitter][:base_uri]}/#{APP_CONFIG[:twitter][:api_version]}/users/show.json?screen_name=#{@given_screen_name}"
     resp = self.class.get("/#{APP_CONFIG[:twitter][:api_version]}/users/show.json?screen_name=#{@given_screen_name}")
     @exists = resp.code.between?(200,299)
     resp
   end
-  
+
   def friends
     puts "Calling: #{APP_CONFIG[:twitter][:base_uri]}/#{APP_CONFIG[:twitter][:api_version]}/statuses/friends/#{@given_screen_name}.json"
     resp = self.class.get("/#{APP_CONFIG[:twitter][:api_version]}/statuses/friends/#{@given_screen_name}.json")
     resp
   end
-  
+
   def followers
     puts "Calling: #{APP_CONFIG[:twitter][:base_uri]}/#{APP_CONFIG[:twitter][:api_version]}/statuses/followers/#{@given_screen_name}.json"
     self.class.get("/#{APP_CONFIG[:twitter][:api_version]}/statuses/followers/#{@given_screen_name}.json")
   end
-  
+
   def all
     self.friends | self.followers
   end
-  
+
   def who(who)
     case who
     when 'friends': self.friends
@@ -66,15 +66,15 @@ class Tweeter
     else []
     end
   end
-  
+
   def <=>(other_tweeter)
     self.screen_name <=> other_tweeter.screen_name
   end
-  
+
   def ==(other_tweeter)
     self.screen_name == other_tweeter.screen_name
   end
-  
+
   def method_missing(name, *args, &block)
 #     puts "Calling #{@tweeter['screen_name']}.#{name}"
     if @tweeter[name.to_s]
